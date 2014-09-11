4cbd92cbcb8b7c914737bbe96e5518da3b4330c8
Uddalak Das <uddalakdas@yahoo.com>
2014-09-12 00:57:55 +0530

    Added contents to the classes

--- src/main/java/com/manning/gia/todo/model/ToDoItem.java
+++ src/main/java/com/manning/gia/todo/model/ToDoItem.java
@@ -1,6 +1,28 @@
 package com.manning.gia.todo.model;
 public class ToDoItem implements Comparable<ToDoItem> {
-private Long id;
-private String name;
-private boolean completed;
+	private Long id;
+	private String name;
+	private boolean completed;
+	
+	public Long getId(){
+		return id;
+	}
+	public void setId(Long id){
+		this.id=id;
+	}
+	public String getName(){
+		return name;
+	}
+	public void setName(String name){
+		this.name=name;
+	}
+	public boolean getCompleted(){
+		return completed;
+	}
+	public void setCompleted(boolean completed){
+		this.completed=completed;
+	}
+	public int compareTo(ToDoItem item){
+		return (int)(this.id-item.id);
+	}
 }
---
+++ src/main/java/com/manning/gia/todo/repository/ToDoRepository.java
@@ -0,0 +1,10 @@
+package com.manning.gia.todo.repository;
+import com.manning.gia.todo.model.ToDoItem;
+import java.util.Collection;
+public interface ToDoRepository {
+	List<ToDoItem> findAll();
+	ToDoItem findById(Long id);
+	Long insert(ToDoItem toDoItem);
+	void update(ToDoItem toDoItem);
+	void delete(ToDoItem toDoItem);
+}
